0eb007aab590c555a622fc5332b14f24f98534d0
kanyakumari <sri.venkatapanuganti@gmail.com>
2026-08-13 13:42:18 +0530

    Fix FHIR client ESM import

--- api/v1/ehr.ts
+++ api/v1/ehr.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { FHIRClient } from "../types/fhir-client.js";
-
+ 
 export default async function handler(
   req: VercelRequest,
   res: VercelResponse
